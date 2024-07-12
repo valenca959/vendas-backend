44ebed664eb8bb39f3e02588a75c412ab055cc08
valenca959 <rodrigovenedino@gmail.com>
2024-07-11 21:30:48 -0300

    feat: adicionando token JWT e criando o login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,20 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { UserEntity } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
 import { LoginDto } from './dtos/login.dto';
 import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { ReturnLogin } from './dtos/returnLogin.dto';
+import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
+import { LoginPayload } from './dtos/loginPayload.dto';
+import { UserEntity } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class AuthService {
 
-    constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService, private jwtService: JwtService) {}
 
-    async login(loginDto: LoginDto): Promise<UserEntity> {
-        const user = await this.userService.findUserByEmail(loginDto.email).catch(() => undefined);
+    async login(loginDto: LoginDto): Promise<ReturnLogin> {
+        const user: UserEntity | undefined = await this.userService.findUserByEmail(loginDto.email).catch(() => undefined);
 
         const isMatch = await compare(loginDto.password, user?.password) || '';
 
@@ -18,7 +22,10 @@ export class AuthService {
             throw new NotFoundException('email ou senha invalidos');
         }
 
-        return user;
+        return {
+            acessToken: this.jwtService.sign({...new LoginPayload(user)}),
+            user: new ReturnUserDto(user),
+        };
         
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
   await app.listen(PORT);
 }
 bootstrap();
